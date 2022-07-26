f540910cc700335eb85295de122ae76fca121768
denys hsu <55130054+cgtinker@users.noreply.github.com>
2022-07-26 19:29:38 +0200

    mediapipe modules

--- hello_world.sh
+++ hello_world.sh
@@ -1,5 +1,2 @@
 export GLOG_logtostderr=1
-echo "Try running Hello World."
-
-bazel run --define MEDIAPIPE_DISABLE_GPU=1 //src/mp/detection:hello_world
-echo "Finished running Hello World."
+bazel run --define MEDIAPIPE_DISABLE_GPU=1 //src/mp/detection/hello_world:hello_world
--- hello_world_graph.sh
+++ hello_world_graph.sh
@@ -1,5 +1,3 @@
-hello_world="deps(//desktop/hello_world:hello_world)"
-# hello_world = "deps(//src:hello_world)" 
-
+hello_world="deps(//src/mp/detection/hello_world:hello_world)"
 bazel query --notool_deps --noimplicit_deps $hello_world --output graph
 
